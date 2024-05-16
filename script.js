8edf555270969ba26c0a828068ca97dac0d816f3
Neeraj Singh <869neerajsingh@gmail.com>
2024-05-17 00:09:49 +0530

    fix: resolve bugs like links not working, variables name not correct, icons not updating properly, etc

--- script.js
+++ script.js
@@ -5,3 +5,53 @@ function toggleMenu() {
   menu.classList.toggle('open');
   icon.classList.toggle('open');
 }
+
+// dark/light mode logic
+const btn = document.getElementById("modeToggle");
+const btn2 = document.getElementById("modeToggle2");
+const themeIcons = document.querySelectorAll(".icon");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+  setDarkMode();
+}
+
+btn.addEventListener("click", function () {
+  console.log("hell");
+  setTheme();
+})
+
+btn2.addEventListener("click", function () {
+  console.log("hell");
+  setTheme();
+})
+
+function setTheme() {
+  const currentTheme = document.body.getAttribute("theme");
+
+  if (currentTheme === "dark") {
+    setLightMode();
+  } else {
+    setDarkMode();
+  }
+}
+
+function setDarkMode() {
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
+
+  themeIcons.forEach(icon => {
+    // in dark mode, light images will be applied
+    icon.src = icon.getAttribute("src-light");
+  })
+}
+
+function setLightMode() {
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
+
+  themeIcons.forEach(icon => {
+    // in light mode, dark images will be applied
+    icon.src = icon.getAttribute("src-dark");
+  })
+}
